7c106017ddac7ba782835407383ce26d96b33579
MelittaPacheco <69293516+MelittaPacheco@users.noreply.github.com>
2020-08-08 15:39:58 +0530

    Create mycaptain ex.cpp
    this is the first ex on my captain application

---
+++ mycaptain ex.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int x ;
+	float y;
+	char z;
+	double w;
+	cout<< " Size of char : "<<sizeof(z) <<" byte"<<endl;
+	cout<< " Size of int : "<< sizeof (x) <<" bytes"<<endl;
+	cout<< " Size of float : "<< sizeof (y) <<" bytes"<<endl;
+	cout<< " Size of double : "<< sizeof (w) <<" bytes"<<endl;
+	
+	return 0 ;
+	
+}
